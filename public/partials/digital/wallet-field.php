a699d20b2cc97e46e3edbed560ebb1496a4a74b7
Ridwan Arifandi <hi@ridwanarifandi.com>
2020-04-29 00:29:33 +0700

    add styling to wallet field

--- public/partials/digital/wallet-field.php
+++ public/partials/digital/wallet-field.php
@@ -1,17 +1,21 @@
 <tr>
     <th colspan='2'>
-        <h4><?php _e('Dana di dompet', 'sejoli'); ?></h4>
-        <p>
-            <label for="use-wallet">
-                <input id='use-wallet' type="checkbox" name="use-wallet" value="">
-                <span>
-                <?php
-                    printf(
-                        __('Dana yang tersedia %s, ini menggunakan dana yang ada untuk pembayaran?', 'sejoli'),
-                        sejolisa_price_format($wallet->available_total)
-                    );
-                ?>
-            </label>
-        </p>
+        <div class="ui info message">
+            <div class="header">
+                <?php _e('Dana di dompet', 'sejoli'); ?>
+            </div>
+            <p style='line-heigh:40px;'>
+                <label for="use-wallet">
+                    <input id='use-wallet' type="checkbox" name="use-wallet" value="">
+                    <span>
+                    <?php
+                        printf(
+                            __('Dana yang tersedia %s, ini menggunakan dana yang ada untuk pembayaran?', 'sejoli'),
+                            sejolisa_price_format($wallet->available_total)
+                        );
+                    ?>
+                </label>
+            </p>
+        </div>
     </th>
 </tr>
--- public/partials/physical/wallet-field.php
+++ public/partials/physical/wallet-field.php
@@ -1,18 +1,22 @@
 <tr>
-    <td colspan='3'>
-        <h4><?php _e('Dana di dompet', 'sejoli'); ?></h4>
-        <p>
-            <label for="use-wallet">
-                <input id='use-wallet' type="checkbox" name="use-wallet" value="">
-                <span>
-                    <?php
-                        printf(
-                            __('Dana yang tersedia %s, ingin menggunakan dana yang ada untuk pembayaran?', 'sejoli'),
-                            sejolisa_price_format($wallet->available_total)
-                        );
-                    ?>
-                </span>
-            </label>
-        </p>
+    <td colspan='3' style='text-align:left;'>
+        <div class="ui info message">
+            <div class="header">
+                <?php _e('Dana di dompet', 'sejoli'); ?>
+            </div>
+            <p style='line-height:40px;'>
+                <label for="use-wallet">
+                    <input id='use-wallet' type="checkbox" name="use-wallet" value="">
+                    <span>
+                        <?php
+                            printf(
+                                __('Dana yang tersedia %s, ingin menggunakan dana yang ada untuk pembayaran?', 'sejoli'),
+                                sejolisa_price_format($wallet->available_total)
+                            );
+                        ?>
+                    </span>
+                </label>
+            </p>
+        </div>
     </td>
 </tr>
